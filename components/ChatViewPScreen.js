a019249a2bac34d33495024cc01394725efc83ab
Franklyn Turbak <fturbak@wellesley.edu>
2024-04-17 22:24:22 -0400

    declare deleteStorageFile to be async

--- components/ChatViewPScreen.js
+++ components/ChatViewPScreen.js
@@ -116,7 +116,7 @@ export default function ChatViewPScreen() {
           + utils.formatJSON(debugObj)); 
   }
 
-  function deleteStorageFile(filename) {
+  function async deleteStorageFile(filename) {
     console.log(`Deleting storage file ${filename} ... `)
     // Create a reference to the file to delete
     const desertRef = ref(storage, filename);
